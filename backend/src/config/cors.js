640fd0ac101784fd2072fcac078f3748141eb8e6
GNANESH6 <sangatignaneshwarreddy2006@gmail.com>
2026-02-06 22:51:52 +0530

    Update cors.js

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 
 export default cors({
-  origin: ["*", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+  origin: ["https://gnanesh-portfolio-delta.vercel.app/"],
   credentials: true
 });
